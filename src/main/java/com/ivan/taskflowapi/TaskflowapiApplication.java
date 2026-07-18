1fc152e00499c3a55cce7a9b915b226854c1e5c2
Ivan Revelino <234983236+ivanrevelino@users.noreply.github.com>
2026-07-18 14:05:11 +0200

    fist commit

---
+++ src/main/java/com/ivan/taskflowapi/TaskflowapiApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.taskflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivan/taskflowapi/TaskflowapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.taskflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
