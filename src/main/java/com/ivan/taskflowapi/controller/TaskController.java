e3afe89eeaaa41b0295c68a00bd26e8df36e722f
Ivan Revelino <234983236+ivanrevelino@users.noreply.github.com>
2026-07-18 23:32:23 +0200

    created TaskController

--- src/main/java/com/ivan/taskflowapi/controller/TaskController.java
+++ src/main/java/com/ivan/taskflowapi/controller/TaskController.java
@@ -1,5 +1,23 @@
 package com.ivan.taskflowapi.controller;
 
+import com.ivan.taskflowapi.dto.task.TaskRequestDTO;
+import com.ivan.taskflowapi.models.Task;
+import com.ivan.taskflowapi.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/projects/{projectId}/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Task> create(@PathVariable Long projectId, @RequestBody TaskRequestDTO dto) {
+        Task task = taskService.create(dto, projectId);
+        return new ResponseEntity<>(task, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/ivan/taskflowapi/service/TaskService.java
+++ src/main/java/com/ivan/taskflowapi/service/TaskService.java
@@ -2,8 +2,11 @@ package com.ivan.taskflowapi.service;
 
 import com.ivan.taskflowapi.dto.task.TaskRequestDTO;
 import com.ivan.taskflowapi.exception.ResourceNotFoundException;
+import com.ivan.taskflowapi.exception.UnauthorizedException;
 import com.ivan.taskflowapi.mapper.TaskMapper;
+import com.ivan.taskflowapi.models.Project;
 import com.ivan.taskflowapi.models.Task;
+import com.ivan.taskflowapi.models.User;
 import com.ivan.taskflowapi.repository.TaskRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -15,20 +18,35 @@ import org.springframework.stereotype.Service;
 public class TaskService {
 
     private final TaskRepository repository;
+    private final ProjectService projectService;
+    private final UserService userService;
     private final TaskMapper taskMapper;
 
     @Transactional
-    public Task create(@Valid  TaskRequestDTO request) {
-        Task entity = taskMapper.toEntity(request);
-        return repository.save(entity);
+    public Task create(@Valid  TaskRequestDTO request, Long projectId) {
+        User owner = userService.getAuthenticatedUser();
+        Project project = projectService.findByIdOrThrowResourceNotFoundException(projectId);
+        verificateIfProjectBelongsToUser(project, owner);
+
+        return Task.builder().title(request.title())
+                .description(request.description())
+                .project(project)
+                .build();
     }
 
     public Task findByIdOrThrowResourceNotFoundException(Long id) {
+
+        User user = userService.getAuthenticatedUser();
+
         return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task Not Found"));
     }
 
     public void delete(Long id) {
         Task taskToBeDeleted = findByIdOrThrowResourceNotFoundException(id);
         repository.delete(taskToBeDeleted);
+    }
+
+    private static void verificateIfProjectBelongsToUser(Project project, User owner) {
+        if (project.getOwner().getId().equals(owner.getId())) throw new UnauthorizedException("\"You don't own this project\"");
     }
 }
